935a73fcae68e55a20d380e0df1ee501620dfa51
Rupakkundu99 <mailupsana@gmail.com>
2025-05-09 15:55:51 +0530

    Error fix

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -9,7 +9,7 @@ const clerkWebhooks=async(req,res)=>{
         //create a svix instance with clerk webhook secret
         const whook=new Webhook(process.env.CLERK_WEBHOOK_SECRET) 
 
-        await whook.verify(JSON.stringify(req,body),{
+        await whook.verify(JSON.stringify(req.body),{
             "svix-id":req.headers["svix-id"],
             "svix-timestamp":req.headers["svix-timestamp"],
             "svix-signature":req.headers["svix-signature"]
