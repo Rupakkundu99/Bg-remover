09a859575e14c7dd60f0995be795dfc82e5ce21c
Rupakkundu99 <mailupsana@gmail.com>
2025-05-09 12:47:16 +0530

    First commit

---
+++ server/controllers/userController.js
@@ -0,0 +1,4 @@
+// API Controller function to manage clerk user with database
+const clerkWebhooks=async(req,res)=>{
+    
+}
---
+++ server/server.js
@@ -0,0 +1,22 @@
+import 'dotenv/config'
+import e from 'express'
+import cors from 'cors'
+import connectDb from './configs/mongodb.js'
+
+//App config
+const PORT=process.env.PORT||5000
+const app=e()
+
+//Initialize middlewares
+app.use(e.json())
+app.use(cors())
+await connectDb()
+
+//API routes
+app.get('/',(req,res)=>{
+    res.send('API is working')
+})
+
+app.listen(PORT,()=>{
+    console.log("Server is runnig on port ",PORT);
+})
